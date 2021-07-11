e6efc40745a8e34ffd169942743f5ffbed2442fc
Nico <nico.nap@outlook.com>
2021-07-11 22:09:10 +0200

    Remove URI

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ app.use(
 //Set up mongoose connection
 var mongoose = require('mongoose');
 // Set up mongoose connection
-var dev_db_url = 'mongodb+srv://cooluser:coolpassword@cluster0-mbdj7.mongodb.net/local_library?retryWrites=true'
-var mongoDB = process.env.MONGODB_URI || dev_db_url;
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
